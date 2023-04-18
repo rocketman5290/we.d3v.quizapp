1896b2358b410ff32306fc6e5ec8c85ae5fdf546
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-04-17 22:39:03 -0400

    Main axis alignment, Container and children, and Rows vs columns

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Cool App'),
-        backgroundColor: Colors.blueGrey[900],
-      ),
-    ));
+            appBar: AppBar(
+              title: const Text('Cool'),
+              backgroundColor: Colors.blueGrey[900],
+            ),
+            body: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.orange,
+                  padding: const EdgeInsets.all(20.0),
+                  child: const Text("I centered Container"),
+                ),
+                Container(
+                  color: Colors.green,
+                  padding: const EdgeInsets.all(15.0),
+                  child: const Icon(Icons.mail),
+                ),
+                Container(
+                  color: Colors.red,
+                  padding: const EdgeInsets.all(15.0),
+                  child: const Icon(Icons.message),
+                ),
+                Container(
+                  color: Colors.blue,
+                  padding: const EdgeInsets.all(15.0),
+                  child: const Icon(Icons.accessibility),
+                ),
+              ],
+            )));
   }
 }
